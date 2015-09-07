d53cf27f0381337f8cca29159d63c70a0c9b02ef
Schmar James <schmarjames@Mac-9.local>
2015-09-07 17:09:38 -0400

    index.js change

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.use('/scripts', express.static(__dirname + '/node_modules/easy-autocomplete/dist/'));
-app.use('/www', express.static(__dirname + '/www/lib'));
-app.use('/bower_components', express.static(__dirname + '/bower_components/));
 
 app.listen(process.env.PORT || 3000);
