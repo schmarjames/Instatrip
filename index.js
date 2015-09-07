3e221c7c1bdf75bf93ed7a66beb931f9b4ee949e
Schmar James <schmarjames@Mac-9.local>
2015-09-07 13:03:23 -0400

    index.js change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var path = require('path');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response) {
     response.sendfile('/home.html');
