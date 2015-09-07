a8084b0f95a4fecd3c034147a2f76e5b83c8e6a1
Schmar James <schmarjames@Mac-9.local>
2015-09-07 11:51:39 -0400

    include express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('home.html');
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + 'public/home.html');
+}).listen(port);
